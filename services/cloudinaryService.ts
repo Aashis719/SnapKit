ca933daa825e20adf68d35f2c524175c718b6e9a
Aashish Neupane <162073333+Aashis719@users.noreply.github.com>
2026-01-06 21:42:33 +0545

    cloudinary services error fixed

--- services/cloudinaryService.ts
+++ services/cloudinaryService.ts
@@ -14,7 +14,7 @@ export const uploadImageToCloudinary = async (file: File) => {
 
     const formData = new FormData();
     formData.append('file', file);
-    formData.append('upload_preset', uploadPreset);
+    formData.append('upload_preset', uploadPreset); 
 
     const response = await fetch(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
         method: 'POST',
